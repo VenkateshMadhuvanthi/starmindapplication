91ac1628a6ab3b36a363f1eb24a9f1f0f5945cb7
VenkateshMadhuvanthi <70901714+VenkateshMadhuvanthi@users.noreply.github.com>
2021-07-26 15:09:01 +0200

    Update levenshtein_calculation.py

--- levenshtein_calculation.py
+++ levenshtein_calculation.py
@@ -2,14 +2,18 @@
 import os
 import glob
 import pandas as pd
+#calculation of distance , reference: https://blog.finxter.com/how-to-calculate-the-levenshtein-distance-in-python/
 def levenshtein(a, b):
     if not a: return len(b)
     if not b: return len(a)
     return min(levenshtein(a[1:], b[1:])+(a[0] != b[0]),
                levenshtein(a[1:], b)+1,
                levenshtein(a, b[1:])+1)
+# retrieving the file as dataframe from url
 sample = pd.read_csv('https://data.stadt-zuerich.ch/dataset/sid_stapo_hundenamen/download/20210103_hundenamen.csv')
+#ignoring the unknown values 
 new_sample= sample[sample.HUNDENAME!= 'unbekannt']
+#printing the names 
 for name in new_sample.HUNDENAME:
      if levenshtein("luca",name) == 1:
         print(name)
